3bf55954638dd024d7f2ca803e80e440772ed1ae
Chukwubuike Kingsley <chukwubuikekingsley369@gmail.com>
2021-10-28 14:59:59 +0100

    king work

---
+++ src/components/folder/file.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+const File = () => {
+    return (
+        <div>
+
+            <div>
+                <div>
+                    <div> Markets</div>
+                    <div>Lend</div>
+                    <div>Synths</div>
+                    <div>Trade</div>
+
+                    <div>
+                         <div>Farm</div>
+                         <div>
+                              <div>lending</div>
+                              <div>Liquidity</div>
+                          </div>
+                    </div>
+                      <div>
+                        <div>Tools</div>
+                           <div>
+                               <div>Liquidation</div>
+                               <div>Price</div>
+                           </div>
+                        </div>
+                        <div>Security</div>
+                        <div>Document</div>
+                        <div>Tutorials</div>
+                        <div>Governance</div>
+                        <div>
+                            <img src="" alt=""/>
+                             <img src="" alt=""/>
+                              <img src="" alt=""/>
+                               <img src="" alt=""/>
+                        </div>
+                        <div>
+                              <img src="" alt=""/>
+                              <img src="" alt=""/>
+                              <img src="" alt=""/>
+                               <img src="" alt=""/>
+                        </div>
+
+                     </div>
+                   
+
+                <div>
+                    <div>
+                        <form>
+                            <div>SEND</div>
+                            <div>
+                                <div>Balance: ... <span>Amount</span></div>
+                                <div>
+                                    <div>
+                                       <img src="" alt=""/>
+                                       <div>USDT </div> 
+                                       <div>MAX</div>
+                                   </div>
+                                   <div><input type="number"  placeholder="0.00"/></div>
+                                 </div>
+
+                                 <div>
+                                     <div>RECEIVE</div>
+                                     <div><img src="" alt=""/></div>
+                                 </div>
+
+                                 <div>
+                                     <div>Balance: ...  <span>Amount</span></div>
+                                     <div>
+                                         <div> <img src="" alt="" /><div>DF </div></div>
+                                         <div><input type="" alt="" /></div>
+                                     </div>
+                                 </div>
+                            </div>
+
+                            <div>Enable Gas Token</div>
+                            <div>Connect Wallet</div>
+                        </form>
+                    </div>
+
+                </div>
+
+                <div>Advanced</div>
+
+               <div>
+
+                   <div>Limit additional price slippage</div>
+                   <div>
+                       <div>0.1%</div>
+                       <div>0.2%</div>
+                       <div>0.5%</div>
+                       <div>1%</div>
+                       <div>3%</div>
+                       <div>5%</div>
+                   </div>
+                   <div>Minimum received:... DF</div>
+                   <div>Gas Fee</div>
+                   <div>
+                       <div>121 Standard</div>  <div>134 Fast </div> <div>148 instant</div>
+                   </div>
+
+               </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default File;
