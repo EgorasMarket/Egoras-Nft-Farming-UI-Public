2e9740a0ad290318bcfd8f1e8e5db7ae9bb691e2
CYNTAX Mac <samuelify225@gmail.com>
2021-11-09 10:25:21 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import Header from "./components/Home/Header";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./components/Home/Home";
+import CountDown from "./components/Home/CountDown";
 // import Footer from "./components/Home/Footer.jsx";
 import "../src/App.css";
 function App() {
@@ -13,8 +14,11 @@ function App() {
       <div className="App-header">
         <Header />
         <Switch>
-          <Route exact path="/">
+          {/* <Route exact path="/">
             <Home />
+          </Route> */}
+          <Route exact path="/">
+            <CountDown />
           </Route>
           <Dashboard />
         </Switch>
