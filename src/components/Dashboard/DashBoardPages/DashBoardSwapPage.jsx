7ce6bfc2e306f2b807f82bfbd90815f20d788d2b
Buike369 <chukwubuikekingsley369@gmail.com>
2021-11-15 08:45:08 -0800

    worked on dropdown search

--- src/components/Dashboard/DashBoardPages/DashBoardSwapPage.jsx
+++ src/components/Dashboard/DashBoardPages/DashBoardSwapPage.jsx
@@ -1,23 +1,70 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import "../../../css/file.css";
 
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
+
 const sweet =[{img:'../../img/name4.png',bar:'ETH',bar1:'Ether'},{img:'../../img/sch.svg',bar:'1INCH',bar1:'1Inch'},{img:'../../img/sch1.svg',bar:'AAVE',bar1:'AaveToken'},{img:'../../img/sch2.svg',bar:'AMP',bar1:'amp'},{img:'../../img/img5.png',bar:'ANT',bar1:'Aragon Network Token'},{img:'../../img/sch4.svg',bar:'BAL',bar1:'Balancer'},{img:'../../img/sch5.png',bar:'BAND',bar1:'Band Protocol'},{img:'../../img/sch7.svg',bar:'BAT',bar1:'BasicAttentionToken'},{img:'../../img/sch9.svg',bar:'BNT',bar1:'Bancor'},{img:'../../img/sch10.svg',bar:'COMP',bar1:'compound'},{img:'../../img/sch13.svg',bar:'CRV',bar1:'CurveDAOToken'},{img:'../../img/sch13.png',bar:'CVC',bar1:'Civic'},{img:'../../img/sch14.svg',bar:'DAI',bar1:'DaiStablecoin'},{img:'../../img/sch16.png',bar:'DNT',bar1:'district0x'},{img:'../../img/sch17.svg',bar:'ENJ',bar1:'Enjin'},{img:'../../img/sch18.jpeg',bar:'ENS',bar1:'Ethereum Name Service'},{img:'../../img/sch19.png',bar:'GNO',bar1:'Gnosis Token'},{img:'../../img/sch20.svg',bar:'GRT',bar1:'The Graph'},{img:'../../img/sch21.svg',bar:'GUSD',bar1:'GeminiDollar'},{img:'../../img/sch22.jpeg',bar:'KEEP',bar1:'Keep Network'},{img:'../../img/name4.png',bar:'KNC',bar1:'KyberNetwork'},{img:'../../img/sch24.svg',bar:'LINK',bar1:'Chainlink'},{img:'../../img/sch25.png',bar:'LOOM',bar1:'Loom Network'},{img:'../../img/sch26.svg',bar:'LRC',bar1:'Looping'},{img:'../../img/sch27.svg',bar:'MANA',bar1:'Decentraland'},{img:'../../img/sch28.svg',bar:'MKR',bar1:'Maker'},{img:'../../img/name4.png',bar:'MLN',bar1:'Melon'},{img:'../../img/sch31.png',bar:'NMR',bar1:'Numeraire'},{img:'../../img/sch32.jpeg',bar:'NU',bar1:'NuCypher'},{img:'../../img/sch33.svg',bar:'OXT',bar1:'Orchid'},{img:'../../img/sch34.svg',bar:'PAXG',bar1:'paxosGold'},{img:'../../img/sch35.svg',bar:'REN',bar1:'Republic'},{img:'../../img/sch36.png',bar:'REP',bar1:'Reputation Angur v1'},{img:'../../img/sch37.png',bar:'REPv2',bar1:'Reputation Angur v2'},{img:'../../img/sch38.svg',bar:'SAND',bar1:'Sandbox'},{img:'../../img/sch39.svg',bar:'SKL',bar1:'Skale'},{img:'../../img/sch40.svg',bar:'SNX',bar1:'Synthetix'},{img:'../../img/sch41.svg',bar:'STORJ',bar1:'Storj'},{img:'../../img/sch42.png',bar:'TBTC',bar1:'tBTC'},{img:'../../img/sch43.svg',bar:'UMA',bar1:'UMAVotingTokenv1'},{img:'../../img/sch44.svg',bar:'UNI',bar1:'Uniswap'},{img:'../../img/sch45.png',bar:'USDC',bar1:'USDCoin'},{img:'../../img/sch46.png',bar:'USDT',bar1:'Tether USD'},{img:'../../img/sch47.png',bar:'WBTC',bar1:'Wrapped BTC'},{img:'../../img/sch49.png',bar:'WETH',bar1:'Wrapped Ether'},{img:'../../img/name4.png',bar:'YFI',bar1:'yearn finance'},{img:'../../img/sch50.svg',bar:'ZRX',bar1:'0xProtocol'}];
+const sweet101 =[{imgs:'../../img/name4.png', nase:'ETH'},{ imgs:'../../img/name2.svg', nase:'DAI'}, { imgs:'../../img/name1.png', nase:'USDC'},{ imgs:'../../img/name2.png', nase:'USDT'},{ imgs:'../../img/name5.png', nase:'WBTC'},{ imgs:'../../img/name6.png', nase:'WETH'}]
+
+ // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+
+    // Get the modal
+var modal = document.getElementById("farm");
+
+    if (event.target == modal) {
+      modal.style.display = "none";
+      document.body.classList.remove('queen');
+    }
+  }
+
+  
 
 const File = () => {
 
     const [ speed, setSpeed] = useState('paper');
     const [ speed1, setSpeed1] = useState('two');
+    const [isOpen,setIsOpen] =useState(true);
+    const [ speed4, setSpeed4] = useState('DF');
+    const [image,setImage] = useState('../../img/map12.png');
 
-   
+    const [changeShow,setChangeShow] = useState(0);
 
+
+
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState([]);
+    const handleChange = event => {
+        setSearchTerm(event.target.value);
+      };
+
+  useEffect(()=>{
+    const results = sweet.filter(person =>
+        person.bar.toLowerCase().includes(searchTerm)
+      );
+      setSearchResults(results);
+
+  },[searchTerm])
+
+
+
+
+    
+//   let defe = (x)=>{
+//  return(x + x)
+//   }
 
  const close4 =()=>{
-     document.getElementById('farm').style.display='none';
+    document.getElementById('farm').style.display='none';
+   
+     document.body.classList.remove('queen');
  }
  const show45 =()=>{
     document.getElementById('farm').style.display='flex';
+    
+
+   document.body.classList.add('queen');
 }
 
 
@@ -79,25 +126,21 @@ const File = () => {
                                      <div className="send2"><span style={{color:'#000'}}>Balance: ...</span>  <span className="send3">Amount</span></div>
                                      <div className="send5">
                                          <div className="send6">
-                                           <div className="send7"> <img src="../../img/map12.png" alt="" className="send4"/></div>
-                                          <div> <div  onClick={show45} style={{display:'flex',alignItems:'center',color:'#000',fontWeight:'600',cursor:'pointer'}}>DF <ArrowDropDownIcon id="COMES1"/>  </div></div>
+                                           <div className="send7"> <img src={image} alt="" className="send4"/></div>
+                                          <div> <div  onClick={show45} style={{display:'flex',alignItems:'center',color:'#000',fontWeight:'600',cursor:'pointer'}}>{speed4} <ArrowDropDownIcon id="COMES1"/>  </div></div>
                                          </div>
-                                         <div><input type="" alt="" className="send8"  placeholder="0.00"/></div>
+                                         <div><input type="number" alt="" className="send8"  placeholder="0.00" value={changeShow} onChange={(ey)=>{setChangeShow(ey.target.value);document.getElementById('send8').style.display="block"}} onClick={()=>{setChangeShow('');}}/>
+                                         <input type="number" alt="" className="send8" id="send8" placeholder="0.00" value={changeShow} />
+                                         </div>
                                      </div>
                                  </div>
                             </div>
                            
-                            {/* <div className="newswap">
-                                <form>
-                                <div><span>Swap</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sc-1ndknrv-0 fZuPAR"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg></swap></div>
-                                <div><div><img src="" alt=""/><span>ETH</span><svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg" class="sc-33m4yg-8 khlnVY"><path d="M0.97168 1L6.20532 6L11.439 1" stroke="#AEAEAE"></path></svg></div> <div><input type="number" placeholder="0.00"/><input type="number" placeholder="0.00"/></div></div>
-                                <div><div>Select a token</div><div><input type="" alt=""/><input type="" alt=""/></div></div>
-                               </form>
-                            </div> */}
+                           
                              
 
                             <div className="Enable">Enable Gas Token</div>
-                            <div className="Enable1"> Connect Wallet</div>
+                            <div className="Enable1" > Connect Wallet</div>
                         </form>
                     </div>
 
@@ -106,7 +149,7 @@ const File = () => {
                  <div id="COMJ"><div className="Enable2 deep" onClick={Just} >Advanced <ArrowDropDownIcon/></div></div>
                  <div id="COMES"><div className="Enable2 deep" onClick={Just2}  >Advanced <ArrowDropDownIcon/></div></div>
 
-               <div className="Enable3" id="water">
+               <div className="Enable3" id="water" >
 
                    <div className="Enable4">Limit additional price slippage</div>
                    <div className="Enable5">
@@ -130,28 +173,29 @@ const File = () => {
 
 
             {/* </div> */}
+            {isOpen ?
 
             <div className="feed12 feed21" id="farm">
                 <div className="feed22">
                     <form>
                         <div className="feed23">
                       <div className="feed25"><div className="feed24">Select a token</div> <svg onClick={close4} xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sc-1cchcrx-1 bNKSgQ"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>
-                      <input type="search" placeholder="Search name or paste address"  className="feed26"/>
+                      <input type="text" placeholder="Search name or paste address"  value={searchTerm} className="feed26" onChange={handleChange}/>
                       <p style={{display:'flex',alignItems:'center',fontWeight:'500'}}>Common bases <span className="feed27">?</span></p>
                       <div className="feed30">
-                          <div className="feed29"><img src="../../img/name4.png" alt="" className="feed28"/><span style={{marginLeft:'8px',fontWeight:'600'}}>ETH</span></div>
-                          <div className="feed29"><img src="../../img/name2.svg" alt="" className="feed28"/><span style={{marginLeft:'8px',fontWeight:'600'}}>DAI</span></div>
-                          <div className="feed29"><img src="../../img/name1.png" alt="" className="feed28"/><span style={{marginLeft:'8px',fontWeight:'600'}}>USDC</span></div>
-                          <div className="feed29"><img src="../../img/name2.png" alt="" className="feed28"/><span style={{marginLeft:'8px',fontWeight:'600'}}>USDT</span></div>
-                          <div className="feed29"><img src="../../img/name5.png" alt="" className="feed28"/><span style={{marginLeft:'8px',fontWeight:'600'}}>WBTC</span></div>
-                          <div className="feed29"><img src="../../img/name6.png" alt="" className="feed28"/><span style={{marginLeft:'8px',fontWeight:'600'}}>WETH</span></div>
+
+                          {sweet101.map(sde =>( <div className="feed29"  onClick={()=>{setSpeed4(sde.nase); document.getElementById('farm').style.display='none';setImage(sde.imgs)}}><img src={sde.imgs} alt="" className="feed28"/><span style={{marginLeft:'8px',fontWeight:'600'}}>{sde.nase}</span></div>))}
+                        
                       </div>
                     </div>
 
                     <div style={{position:'relative',flex:1,overflow:'auto'}}>
                     <div className="saw2">
 
-                        {sweet.map(ser=>(<div className='saw' style={{display:'flex',alignItems:'center',marginBottom:'10px'}}><img src={ser.img} alt="" className="feed28"/><div style={{display:'flex',marginLeft:'10px',flexDirection:'column'}}><span>{ser.bar}</span><span style={{fontSize:'12px',marginTop:'-5px',fontWeight:'300',color:'rgb(110, 114, 125)'}}>{ser.bar1}</span></div></div>))}
+                        {/* {sweet.map((ser)=>(<div  onClick={()=>{setSpeed4(ser.bar); document.getElementById('farm').style.display='none'; setImage(ser.img)}} key={ser}className='saw' style={{display:'flex',alignItems:'center',marginBottom:'10px'}}><img src={ser.img} alt="" className="feed28"/><div style={{display:'flex',marginLeft:'10px',flexDirection:'column'}}><span>{ser.bar}</span><span style={{fontSize:'12px',marginTop:'-5px',fontWeight:'300',color:'rgb(110, 114, 125)'}}>{ser.bar1}</span></div></div>)) */}
+                                             {searchResults.map((ser)=>(<div  onClick={()=>{setSpeed4(ser.bar); document.getElementById('farm').style.display='none'; setImage(ser.img)}} key={ser}className='saw' style={{display:'flex',alignItems:'center',marginBottom:'10px'}}><img src={ser.img} alt="" className="feed28"/><div style={{display:'flex',marginLeft:'10px',flexDirection:'column'}}><span>{ser.bar}</span><span style={{fontSize:'12px',marginTop:'-5px',fontWeight:'300',color:'rgb(110, 114, 125)'}}>{ser.bar1}</span></div></div>))
+
+                        }
 
 
                       </div>
@@ -161,11 +205,14 @@ const File = () => {
                     </form>
                     <div className='saw1'>Manage Token Lists</div>
                 </div>
-            </div>
+            </div> :null
 
-            
+          }    
+
         </div>
     )
 }
+
+
 
 export default File;
