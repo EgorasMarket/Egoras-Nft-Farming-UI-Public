70521c584c67abc7864dde830721959330b575d0
Apple <samuelify225@gmail.com>
2022-06-23 15:10:44 +0100

    update

--- src/components/Dashboard/DashBoardPages/OpenVaultPage.jsx
+++ src/components/Dashboard/DashBoardPages/OpenVaultPage.jsx
@@ -624,7 +624,7 @@ const OpenVaultPage = ({ match }) => {
             <div className="vault_captions">
               <p className="vault_tbd">
                 VaultID
-                <span className="vault_percent">T.B.D</span>
+                <span className="vault_percent">E.C.V</span>
               </p>
 
               <p className="vault_tbd">
@@ -632,7 +632,7 @@ const OpenVaultPage = ({ match }) => {
               </p>
 
               <p className="vault_tbd">
-                Liquidation Fee <span className="vault_percent">35%</span>
+                Liquidation Fee <span className="vault_percent">13%</span>
               </p>
 
               <p className="vault_tbd">
