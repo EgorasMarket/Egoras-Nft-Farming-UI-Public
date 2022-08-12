f46b4d91343be0b238ce37639c3d4679c29d1414
Apple <samuelify225@gmail.com>
2022-08-12 10:00:52 +0100

    update

--- src/components/Dashboard/DashBoardPages/DashBoardLendPage.jsx
+++ src/components/Dashboard/DashBoardPages/DashBoardLendPage.jsx
@@ -140,7 +140,7 @@ const DashBoardLendPage = ({ submitKyc, auth }) => {
         let response = await getAuthUserStats(account);
         // console.log(response.message.data.payload, "acct acct acct acct ");
         const payload = response.message.data.payload;
-        console.log(payload, "acct acct acct acct ");
+        // console.log(payload, "acct acct acct acct ");
         setAddress(account);
         setConnected(() => false);
         if (payload == null) {
