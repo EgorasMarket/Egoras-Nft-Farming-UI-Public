705804da919f70171be2aeac1a0dd9cf025f0a78
Kingsley Goodluck <goodluckcanhelp@gmail.com>
2023-03-17 17:10:43 +0100

    update

--- src/core/ApiRoutes.js
+++ src/core/ApiRoutes.js
@@ -3,4 +3,5 @@ import { API_URL } from "../actions/types";
 export const GET_NEW_PRODUCTS_ROUTE = `${API_URL}/product/new/zero-tradable`;
 export const POPULATE_ADMIN_PRODUCT_DASHBOARD_ROUTE = `${API_URL}/product/admin/dashboard/product`;
 export const GET_ALL_UPLOADED_PRODUCTS_ROUTE = `${API_URL}/product/uploaded`;
+export const GET_ALL_UPLOADED_PRODUCTS_FOR_USER_ROUTE = `${API_URL}/product/uploaded`;
 export const FETCH_TRADABLE_PRODUCTS_ROUTE = `${API_URL}/product/approved/tradable-true`;
