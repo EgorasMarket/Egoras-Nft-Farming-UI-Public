24fce4c234b11445359f19ad13d287d685b21f7a
“Chidubem <chidubem@rawdev.com.ng>
2023-03-20 16:43:33 +0100

    working

--- src/web3/index2.js
+++ src/web3/index2.js
@@ -2,12 +2,12 @@ import { Contract } from "@ethersproject/contracts";
 import MembershipFacet from "./contracts/V3/MembershipFacet.json";
 import V3ContractAddress from "./contracts/V3/V3ContractAddress.json";
 const contractMembershipFacetInstance = async (signer) => {
-  const contract = await new Contract(
+  return new Contract(
     V3ContractAddress.address,
     MembershipFacet.abi,
     signer
   );
-  return contract;
+  
 };
 const monthlyPlanSubScribe = async (signer) => {
   // try {
@@ -28,16 +28,23 @@ const monthlyPlanSubScribe = async (signer) => {
   //   };
   // }
   try {
+
+    
     const instance = await contractMembershipFacetInstance(signer);
+//  let res = await instance.configurePlan("5000000000000000000","10000000000000000000","15000000000000000000",
+//       "0x133e87c6fe93301c3c4285727a6f2c73f50b9c19",
+//       "0x58f66d0183615797940360a43c333a44215830ba",
+//     );
+    let res = await instance.membershipMonthlyPlan();
+    
     //   monthlyPlan;
     // instance.Contract.monthlyPlan();
     // console.log(instance, "instance");
     // console.log(instance.monthlyPlan(), "instance.monthlyPlan");
     let result;
-    result = await instance.monthlyPlan(
-      "0x1DEDA7AC812c8D5fe5cd39FcD520B8C8271F4768"
-    );
-    console.log(result, "the result ");
+    //result = await instance.methods.getNextSpill();
+
+    console.log(res, "the result ");
     // console.log(typeof instance, "typeOf instance");
   } catch (error) {
     console.log(error);
