f9c5c1e7e49ca2dd0667b607706ad0cd548923b4
Ebrinix <ebrinix121@yahoo.com>
2023-04-12 11:18:11 +0100

    Update

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -178,10 +178,29 @@ const Header = ({ togglemakeDark, check }) => {
   const avatarRef = useRef();
 
   useEffect(() => {
+    console.log(account, avatarRef, walletAddr, "pppooooo");
     // setWalletAddr(account);
     // // console.log(walletAddr.slice(0, 10));
     const element = avatarRef.current;
     if (element && account) {
+      setWalletAddr(account);
+      setConnectTxt("Connected");
+      const addr = account.slice(2, 10);
+      const seed = parseInt(addr, 16);
+      console.log(addr, seed);
+      const icon = jazzicon(20, seed); //generates a size 20 icon
+      if (element.firstChild) {
+        element.removeChild(element.firstChild);
+      }
+      element.appendChild(icon);
+    }
+  }, [account, avatarRef, walletAddr]);
+
+  useEffect(() => {
+    console.log(account, "pppooooo");
+    // setWalletAddr(account);
+    // // console.log(walletAddr.slice(0, 10));
+    if (account) {
       const fetchData = async () => {
         const response = await CALL_CHECK_USER_AND_MEMBERSHIP(account);
         console.log(response.data);
@@ -195,19 +214,9 @@ const Header = ({ togglemakeDark, check }) => {
       };
 
       fetchData();
-
-      setWalletAddr(account);
-      setConnectTxt("Connected");
-      const addr = account.slice(2, 10);
-      const seed = parseInt(addr, 16);
-      console.log(addr, seed);
-      const icon = jazzicon(20, seed); //generates a size 20 icon
-      if (element.firstChild) {
-        element.removeChild(element.firstChild);
-      }
-      element.appendChild(icon);
     }
-  }, [account, avatarRef, walletAddr]);
+  }, [account]);
+
   const lightSet = () => {
     // if (darkMode)
     console.log("dark");
