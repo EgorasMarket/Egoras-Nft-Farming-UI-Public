87740372f3dde1ac09a01d8974d2a4207e9a39bd
Ebrinix <ebrinix121@yahoo.com>
2023-05-01 10:22:53 +0100

    Ready for deployment

--- build/static/js/5.2bab7f84.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnft-app"]=this["webpackJsonpnft-app"]||[]).push([[5],{1318:function(p,n){}}]);
-//# sourceMappingURL=5.2bab7f84.chunk.js.map
---
+++ build/static/js/5.f631c043.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnft-app"]=this["webpackJsonpnft-app"]||[]).push([[5],{1321:function(p,n){}}]);
+//# sourceMappingURL=5.f631c043.chunk.js.map
