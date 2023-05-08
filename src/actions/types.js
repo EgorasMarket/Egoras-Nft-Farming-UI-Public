62b994c4cf090200c6ab56812e20043d06c02fcb
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-05-08 12:30:03 +0100

    justc

--- src/actions/types.js
+++ src/actions/types.js
@@ -7,3 +7,4 @@ export const CANCELLED = "CANCELLED";
 // export const API_URL = "http://192.168.212.63:8000";
 // export const API_URL = "http://localhost:8000";
 export const API_URL = "https://egoras-v3-staging.egoras.com";
+//new comment 
