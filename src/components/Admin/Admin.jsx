0e09aa772fe5bafd75b2f30986aff73b73163067
Ebrinix <ebrinix121@yahoo.com>
2023-05-11 00:43:51 +0100

    Update

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -13,6 +13,7 @@ import AdminSeeSellers from "./AdminPages/AdminSeeSellers";
 import AdminStaffPage from "./AdminPages/AdminStaffPage";
 import AdminModifyMembership from "./AdminPages/AdminModifyMembership";
 import AdminProductsPage from "./AdminPages/AdminProductsPage";
+import AdminProductOrder from "./AdminPages/AdminProductOrder";
 // import AdminMinorPage from "./AdminMinorPage";
 import DashBoardUserDetails from "../Dashboard/DashBoardPages/DashBoardUserDetails";
 import PriceOracle from "./AdminPages/AdminSetRouter/PriceOracle";
@@ -45,6 +46,11 @@ const Admin = ({ check, togglemakeDark }) => {
                 exact
                 path="/admin/all/products"
                 component={AdminProductsPage}
+              />
+              <Route
+                exact
+                path="/admin/all/orders"
+                component={AdminProductOrder}
               />
               {/* <Route
                 exact
