bf928389257febdac5206575f77393ecf5ea433b
Ebrinix <ebrinix121@yahoo.com>
2023-05-21 17:01:02 +0100

    update

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,6 +5,6 @@ export const PENDING = "PENDING";
 export const COMPLETED = "COMPLETED";
 export const CANCELLED = "CANCELLED";
 // export const API_URL = "http://192.168.212.63:8000";
-export const API_URL = "http://localhost:8000";
-// export const API_URL = "https://egoras-v3-staging.egoras.com";
+// export const API_URL = "http://localhost:8000";
+export const API_URL = "https://egoras-v3-staging.egoras.com";
     
