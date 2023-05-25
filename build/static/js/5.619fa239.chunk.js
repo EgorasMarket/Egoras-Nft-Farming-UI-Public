67f63bfc768760799fd1dd2d3bb705954089816d
Ebrinix <ebrinix121@yahoo.com>
2023-05-25 15:16:41 +0100

    Deployment To Mainnet

--- build/static/js/5.619fa239.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnft-app"]=this["webpackJsonpnft-app"]||[]).push([[5],{1326:function(p,n){}}]);
-//# sourceMappingURL=5.619fa239.chunk.js.map
---
+++ build/static/js/5.f095988d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnft-app"]=this["webpackJsonpnft-app"]||[]).push([[5],{1332:function(p,n){}}]);
+//# sourceMappingURL=5.f095988d.chunk.js.map
